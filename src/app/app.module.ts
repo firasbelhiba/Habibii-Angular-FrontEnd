8712acae6908ca0101021f8a3d4d8186fb328d8b
firasbelhiba <firas.belhiba@esprit.tn>
2021-01-12 01:01:35 +0100

    Navbar Html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ValueComponent
+    ValueComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
